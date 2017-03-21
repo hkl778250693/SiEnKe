aeca450dcf413d0cfbfefee27ca51864096bff17
Ranhaihan <594576783@qq.com>
2017-03-21 15:46:47 +0800

    优化

--- MyApplicationSienke/app/src/main/java/com/example/administrator/myapplicationsienke/activity/NewTaskActivity.java
+++ MyApplicationSienke/app/src/main/java/com/example/administrator/myapplicationsienke/activity/NewTaskActivity.java
@@ -1,18 +1,45 @@
 package com.example.administrator.myapplicationsienke.activity;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.administrator.myapplicationsienke.MainActivity;
 import com.example.administrator.myapplicationsienke.R;
+
+import java.util.Calendar;
 
 /**
  * Created by Administrator on 2017/3/15.
  */
 public class NewTaskActivity extends Activity {
+    private TextView securityCheckCase, securityHiddenReason;//安检区域 安检类型
+    private TextView date;//开始日期选择器
+    private TextView date1;//结束日期选择器
+    private int year;
+    private int month;
+    private int day;
+    private int year1;
+    private int month1;
+    private int day1;
+    private RadioButton notSecurityCheck, passSecurityCheck, notPassSecurityCheck, overSecurityCheckTime;
+    private RadioButton indoorStandPipe, indoorBranchPipe, fuelGasMeter, burningAppliances, gasFacilitiesRoom, threeWayPipe;
+    private LayoutInflater inflater;  //转换器
+    private View securityCaseView, securityHiddenreasonView;
+    private PopupWindow popupWindow;
     private ImageView newTaskBack;
     private Button newPlanAddBtn;
 
@@ -25,16 +52,36 @@ public class NewTaskActivity extends Activity {
         setViewClickListener();//点击事件
     }
 
+
+    //强制竖屏
+    @Override
+    protected void onResume() {
+        if (getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+        super.onResume();
+    }
+
+
     //绑定控件
     private void bindView() {
         newTaskBack = (ImageView) findViewById(R.id.newtask_back);
         newPlanAddBtn = (Button) findViewById(R.id.newplan_add_btn);
+        securityCheckCase = (TextView) findViewById(R.id.security_check_case);
+        securityHiddenReason = (TextView) findViewById(R.id.security_hidden_reason);
+        date = (TextView) findViewById(R.id.data);
+        date1= (TextView) findViewById(R.id.data1);
+
     }
 
     //点击事件
-    private void setViewClickListener(){
+    private void setViewClickListener() {
         newTaskBack.setOnClickListener(onClickListener);
         newPlanAddBtn.setOnClickListener(onClickListener);
+        securityCheckCase.setOnClickListener(onClickListener);
+        securityHiddenReason.setOnClickListener(onClickListener);
+        date.setOnClickListener(onClickListener);
+        date1.setOnClickListener(onClickListener);
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -49,9 +96,199 @@ public class NewTaskActivity extends Activity {
                     startActivity(intent1);
                     finish();
                     break;
+                case R.id.security_check_case:
+                    createSecurityCasePopupwindow();
+                    break;
+                case R.id.security_hidden_reason:
+                    createSecurityHiddenReasonPopupwindow();
+                    break;
+                case R.id.data:
+                    Calendar cale1 = Calendar.getInstance();
+                    year = cale1.get(Calendar.YEAR);
+                    month = cale1.get(Calendar.MONTH);
+                    day = cale1.get(Calendar.DAY_OF_MONTH);
+                    date.setText(year + "-" + (month + 1) + "-" + day);
+                    //开始时间选择器
+                    new DatePickerDialog(NewTaskActivity.this, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int myyear, int monthOfYear,
+                                              int dayOfMonth) {
+                            year = myyear;
+                            month = monthOfYear;
+                            day = dayOfMonth;
+                            updateDate();
+                        }
+
+                        private void updateDate() {
+                            date.setText(year + "-" + (month + 1) + "-" + day);
+                        }
+                    }
+                            , cale1.get(Calendar.YEAR)
+                            , cale1.get(Calendar.MONTH)
+                            , cale1.get(Calendar.DAY_OF_MONTH)).show();
+                    break;
+                case R.id.data1:
+                    Calendar cale2 = Calendar.getInstance();
+                    year1 = cale2.get(Calendar.YEAR);
+                    month1 = cale2.get(Calendar.MONTH);
+                    day1 = cale2.get(Calendar.DAY_OF_MONTH);
+                    date1.setText(year + "-" + (month + 1) + "-" + day);
+                    //结束时间选择器
+                    new DatePickerDialog(NewTaskActivity.this, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int myyear, int monthOfYear,
+                                              int dayOfMonth) {
+                            year = myyear;
+                            month = monthOfYear;
+                            day = dayOfMonth;
+                            updateDate();
+                        }
+
+                        private void updateDate() {
+                            date1.setText(year + "-" + (month + 1) + "-" + day);
+                        }
+                    }
+                            , cale2.get(Calendar.YEAR)
+                            , cale2.get(Calendar.MONTH)
+                            , cale2.get(Calendar.DAY_OF_MONTH)).show();
+                    break;
             }
         }
     };
+
+
+    //弹出安检区域popupwindow
+    public void createSecurityCasePopupwindow() {
+        inflater = LayoutInflater.from(NewTaskActivity.this);
+        securityCaseView = inflater.inflate(R.layout.popupwidow_security_area, null);
+        popupWindow = new PopupWindow(securityCaseView, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        //绑定控件ID
+        notSecurityCheck = (RadioButton) securityCaseView.findViewById(R.id.not_security_check);
+        passSecurityCheck = (RadioButton) securityCaseView.findViewById(R.id.pass_security_check);
+        notPassSecurityCheck = (RadioButton) securityCaseView.findViewById(R.id.not_pass_security_check);
+        overSecurityCheckTime = (RadioButton) securityCaseView.findViewById(R.id.over_security_check_time);
+        //设置点击事件
+        notSecurityCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+                securityCheckCase.setText(notSecurityCheck.getText());
+            }
+        });
+        passSecurityCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+                securityCheckCase.setText(passSecurityCheck.getText());
+            }
+        });
+        notPassSecurityCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+                securityCheckCase.setText(notPassSecurityCheck.getText());
+            }
+        });
+        overSecurityCheckTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+                securityCheckCase.setText(overSecurityCheckTime.getText());
+            }
+        });
+        popupWindow.setFocusable(true);
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.update();
+        popupWindow.setBackgroundDrawable(getResources().getDrawable(R.color.transparent));
+        popupWindow.setAnimationStyle(R.style.Popupwindow);
+        backgroundAlpha(0.8F);   //背景变暗
+        popupWindow.showAsDropDown(securityCheckCase, 490, 0);
+        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                backgroundAlpha(1.0F);
+            }
+        });
+    }
+
+    //弹出安全隐患类型popupwindow
+    public void createSecurityHiddenReasonPopupwindow() {
+        inflater = LayoutInflater.from(NewTaskActivity.this);
+        securityHiddenreasonView = inflater.inflate(R.layout.popupwindow_security_type, null);
+        popupWindow = new PopupWindow(securityHiddenreasonView, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        //绑定控件ID
+        indoorStandPipe = (RadioButton) securityHiddenreasonView.findViewById(R.id.indoor_stand_pipe);
+        indoorBranchPipe = (RadioButton) securityHiddenreasonView.findViewById(R.id.indoor_branch_pipe);
+        fuelGasMeter = (RadioButton) securityHiddenreasonView.findViewById(R.id.fuel_gas_meter);
+        burningAppliances = (RadioButton) securityHiddenreasonView.findViewById(R.id.burning_appliances);
+        gasFacilitiesRoom = (RadioButton) securityHiddenreasonView.findViewById(R.id.gas_facilities_room);
+        threeWayPipe = (RadioButton) securityHiddenreasonView.findViewById(R.id.three_way_pipe);
+        //设置点击事件
+        indoorStandPipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+                securityHiddenReason.setText(indoorStandPipe.getText());
+            }
+        });
+        indoorBranchPipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+                securityHiddenReason.setText(indoorBranchPipe.getText());
+            }
+        });
+        fuelGasMeter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+                securityHiddenReason.setText(fuelGasMeter.getText());
+            }
+        });
+        burningAppliances.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+                securityHiddenReason.setText(burningAppliances.getText());
+            }
+        });
+        gasFacilitiesRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+                securityHiddenReason.setText(gasFacilitiesRoom.getText());
+            }
+        });
+        threeWayPipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+                securityHiddenReason.setText(threeWayPipe.getText());
+            }
+        });
+        popupWindow.setFocusable(true);
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.update();
+        popupWindow.setBackgroundDrawable(getResources().getDrawable(R.color.transparent));
+        popupWindow.setAnimationStyle(R.style.Popupwindow);
+        backgroundAlpha(0.8F);   //背景变暗
+        popupWindow.showAsDropDown(securityHiddenReason, 365, 0);
+        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                backgroundAlpha(1.0F);
+            }
+        });
+
+    }
+
+
+    //设置背景透明度
+    public void backgroundAlpha(float bgAlpha) {
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.alpha = bgAlpha; //0.0-1.0
+        getWindow().setAttributes(lp);
+    }
 
 
 }
