90c299d346255a7033fbac2f0270a3e7c244ceba
hkl778250693 <778250693@qq.com>
2017-04-12 09:13:55 +0800

    没得撒子

--- MyApplicationSienke/app/src/main/java/com/example/administrator/myapplicationsienke/activity/MobileSecurityLoginActivity.java
+++ MyApplicationSienke/app/src/main/java/com/example/administrator/myapplicationsienke/activity/MobileSecurityLoginActivity.java
@@ -121,13 +121,13 @@ public class MobileSecurityLoginActivity extends Activity {
                             ip = sharedPreferences.getString("security_ip", "");
                             //Log.i("sharedPreferences=ip=>",ip);
                         } else {
-                            ip = "88.88.88.31:";
+                            ip = "88.88.88.66:";
                         }
                         if (!sharedPreferences.getString("security_port", "").equals("")) {
                             port = sharedPreferences.getString("security_port", "");
                             //Log.i("sharedPreferences=ip=>",port);
                         } else {
-                            port = "8080";
+                            port = "8088";
                         }
                         String httpUrl = "http://" + ip + port + "/SMDemo/login.do";
                         Log.i("httpUrl==========>", "" + httpUrl);
